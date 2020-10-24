3bec10a2d2dfbca104863cce61ca0d6d44e668c6
Brian Guan <brian@guan.us>
2020-10-24 00:10:55 -0700

    Moved notebooks into nbs subdir

--- run_subcoco_ivf.py
+++ run_subcoco_ivf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 from mcbbox.subcoco_ivf import *
 
-img_sz, bs, acc, head_runs, full_runs, lr = 512, 4, 8, 20, 100, 0.01
+img_sz, bs, acc, head_runs, full_runs, lr = 512, 3, 12, 20, 100, 0.01
 tfms, learn, backbone_name = gen_transforms_and_learner(img_size=img_sz, bs=bs, acc_cycs=acc)
 run_training(learn, min_lr=lr, head_runs=head_runs, full_runs=full_runs)
 save_model_fpath = f'models/{backbone_name}-subcoco-{img_sz}-final.pth'
