ec2cc56d34e7dc159d316f60f968ebc2ed249b23
Brian Guan <brian@guan.us>
2020-10-25 23:38:48 -0700

    Mod run script to run real production training settings

--- run_subcoco_ivf.py
+++ run_subcoco_ivf.py
@@ -7,10 +7,10 @@ train_json = fetch_subcoco(datadir=datadir, url=url, img_subdir=img_subdir)
 stats = load_stats(train_json, img_dir=f'{datadir}/coco_sample/{img_subdir}', force_reload=False)
 train_records, valid_records = parse_subcoco(stats)
 
-img_sz=128
-tfms, learn, backbone_name = gen_transforms_and_learner(stats, train_records, valid_records, img_size=img_sz, bs=2, acc_cycs=16)
-run_training(learn, min_lr=0.01, head_runs=1, full_runs=1)
+img_sz=512
+tfms, learn, backbone_name = gen_transforms_and_learner(stats, train_records, valid_records, img_sz=img_sz, bs=3, acc_cycs=12)
+run_training(learn, min_lr=0.01, head_runs=20, full_runs=200)
 
-save_model_fpath = f'models/{backbone_name}-subcoco-{img_sz}-final.pth'
+save_model_fpath = f'models/{backbone_name}-subcoco-{img_sz}-220-final.pth'
 save_final(learn, save_model_fpath)
 
--- run_subcoco_pl.py
+++ run_subcoco_pl.py
@@ -7,7 +7,7 @@ train_json = fetch_subcoco(datadir=datadir, url=url, img_subdir=img_subdir)
 img_dir = f'{datadir}/coco_sample/{img_subdir}'
 stats = load_stats(train_json, img_dir=img_dir, force_reload=False)
 
-img_sz=128
-frcnn_model = run_training(stats, img_dir, img_sz=img_sz, bs=2, acc=2, workers=2, head_runs=1, full_runs=1)
-model_save_path = f"models/FRCNN-subcoco-{img_sz}-final.saved"
+img_sz=512
+frcnn_model = run_training(stats, img_dir, img_sz=img_sz, bs=2, acc=16, workers=4, head_runs=20, full_runs=200)
+model_save_path = f"models/FRCNN-subcoco-{img_sz}-220-final.saved"
 save_final(frcnn_model, model_save_path)
