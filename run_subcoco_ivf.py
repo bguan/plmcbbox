d1342bb4aad97ba5b5d9b275bf4e48d2bd4fa5f3
Brian Guan <brian@guan.us>
2020-10-27 23:48:57 -0700

    Tweak run scripts. Clamp pytorch to be 1.6.x to avoid failures

--- run_subcoco_ivf.py
+++ run_subcoco_ivf.py
@@ -2,14 +2,18 @@
 from mcbbox.subcoco_utils import *
 from mcbbox.subcoco_ivf import *
 
-datadir, url, img_subdir = 'workspace', 'https://s3.amazonaws.com/fast-ai-coco/coco_sample.tgz', 'train_sample'
+datadir, url, froot, img_subdir = 'workspace', 'http://files.fast.ai/data/examples/coco_tiny.tgz', 'coco_tiny', 'train'
+
+# datadir, url, froot, img_subdir = 'workspace', 'https://s3.amazonaws.com/fast-ai-coco/coco_sample.tgz', 'coco_sample', 'train_sample'
 train_json = fetch_subcoco(datadir=datadir, url=url, img_subdir=img_subdir)
-stats = load_stats(train_json, img_dir=f'{datadir}/coco_sample/{img_subdir}', force_reload=False)
+img_dir = f'{datadir}/{froot}/{img_subdir}'
+stats = load_stats(train_json, img_dir=img_dir, force_reload=False)
+
 train_records, valid_records = parse_subcoco(stats)
 
-img_sz=512
+img_sz=128
 tfms, learn, arch = gen_transforms_and_learner(stats, train_records, valid_records, img_sz=img_sz, bs=4, acc_cycs=8)
-run_training(learn, min_lr=0.01, head_runs=20, full_runs=200)
+run_training(learn, min_lr=0.01, head_runs=1, full_runs=1)
 
 save_model_fpath = f'models/{arch}-subcoco-{img_sz}-220-final.pth'
 save_final(learn, save_model_fpath)
--- run_subcoco_pl.py
+++ run_subcoco_pl.py
@@ -2,14 +2,14 @@
 from mcbbox.subcoco_utils import *
 from mcbbox.subcoco_pl import *
 
-datadir, url, img_subdir = 'workspace', 'http://files.fast.ai/data/examples/coco_tiny.tgz', 'train'
+datadir, url, froot, img_subdir = 'workspace', 'http://files.fast.ai/data/examples/coco_tiny.tgz', 'coco_tiny', 'train'
 
-# datadir, url, img_subdir = 'workspace', 'https://s3.amazonaws.com/fast-ai-coco/coco_sample.tgz', 'train_sample'
+# datadir, url, froot, img_subdir = 'workspace', 'https://s3.amazonaws.com/fast-ai-coco/coco_sample.tgz', 'coco_sample', 'train_sample'
 train_json = fetch_subcoco(datadir=datadir, url=url, img_subdir=img_subdir)
-img_dir = f'{datadir}/coco_sample/{img_subdir}'
+img_dir = f'{datadir}/{froot}/{img_subdir}'
 stats = load_stats(train_json, img_dir=img_dir, force_reload=False)
 
 img_sz=128
-frcnn_model = run_training(stats, img_dir, img_sz=img_sz, bs=1, acc=16, workers=1, head_runs=1, full_runs=0)
-model_save_path = f"models/FRCNN-subcoco-{img_sz}-220-final.saved"
+frcnn_model, last_save_fname = run_training(stats, 'models', img_dir, img_sz=img_sz, bs=1, acc=16, workers=1, head_runs=1, full_runs=0)
+model_save_path = f"models/FRCNN-{froot}-{img_sz}-final.saved"
 save_final(frcnn_model, model_save_path)
